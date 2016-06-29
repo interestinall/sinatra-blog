df9f1ed09efdafee58445d6ca35015f28340dd70
interestinall <justin.lefurjah@gmail.com>
2016-06-29 07:58:31 -0500

    redirect to user post index if user tries to signup but is already logged in, write better error message for failed signup

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,42 +3,44 @@ require 'rack-flash'
 class UserController < ApplicationController
   use Rack::Flash
 
-  get '/signup' do 
+  get '/signup' do
     if !session[:id]
       haml :'users/signup'
+    else
+      redirect "/#{user.username}/posts"
     end
   end
 
-  post '/signup' do 
+  post '/signup' do
     @user = User.new(params[:user])
     if @user.save
-      session[:id] = @user.id 
+      session[:id] = @user.id
       redirect "/users/#{@user.id}"
-    else 
-      flash[:message] = "Make sure to fill in all the fields."
+    else
+      flash[:message] = "Make sure to fill in all the fields and that you don't already have an account with the email you are using."
       redirect '/signup'
     end
   end
 
-  get '/users/:id' do 
+  get '/users/:id' do
     @user = User.find(params[:id])
     haml :'users/show'
   end
 
-  get '/login' do 
+  get '/login' do
     if !session[:id]
       haml :'users/login'
-    else 
+    else
       #TODO redirect somehwere better, maybe a list of their post titles
       user = User.find(session[:id])
       redirect "/#{user.username}/posts"
     end
   end
 
-  post '/login' do 
+  post '/login' do
     user = User.find_by(username: params[:user][:username])
     if user && user.authenticate(params[:user][:password])
-      session[:id] = user.id 
+      session[:id] = user.id
       redirect "/users/#{user.id}"
     else
       flash[:message] = "Invalid data. Sure you have an account?"
@@ -46,12 +48,12 @@ class UserController < ApplicationController
     end
   end
 
-  get '/logout' do 
+  get '/logout' do
     if !session[:id].nil?
       session.clear
     end
     redirect '/'
   end
-    
+
 
 end
